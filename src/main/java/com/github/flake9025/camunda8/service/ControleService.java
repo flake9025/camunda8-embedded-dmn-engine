f633b1a6de549f42ce6d7571804c3d0050e4df28
AC75008762 <vincent.villain@acoss.fr>
2024-05-23 17:12:40 +0200

    clean

--- src/main/java/com/github/flake9025/camunda8/service/ControleService.java
+++ src/main/java/com/github/flake9025/camunda8/service/ControleService.java
@@ -39,7 +39,7 @@ public class ControleService {
         List<ResultatControle> resultats = new ArrayList<>();
         CamundaService.asJavaStream(controle.getParsedDmn().decisions()).forEach(dmnDecision -> {
             // on filtre les variables d'entrée vraiment utilisées par la table de décision
-            Map<String, Object> camundaVariables = getDmnInputsAndValues(dmnDecision, dmnVariables);
+            Map<String, Object> camundaVariables = buildDecisionInputsAndValues(dmnDecision, dmnVariables);
             VariablesContext variablesContext = new VariablesContext(camundaVariables);
 
             // Execution de la table de décision (avec les variables réduites)
@@ -59,24 +59,6 @@ public class ControleService {
                 log.error(dmnDecisionResult.getFailureMessage());
             }
 
-            // Filtrage des parametres du referentiel
-            List<String> expressions = camundaService.getDecisionExpressions(dmnDecision);
-            if(camundaVariables.containsKey("RefParamMPC")) {
-                Map<String, Object> refParamMap = (Map<String, Object>) camundaVariables.get("RefParamMPC");
-                Map<String, Object> refParamMapFiltered = new HashMap<>();
-                expressions.forEach(expression -> refParamMap.forEach((key, value) -> {
-                            if (expression.contains("RefParamMPC." + key)) {
-                                log.debug("Decision '{}' : expression '{}' uses key '{}'", dmnDecision.id(), expression, "RefParamMPC." + key);
-                                if (!refParamMapFiltered.containsKey(key)) {
-                                    refParamMapFiltered.put(key, value);
-                                }
-                            }
-                        })
-                );
-                // remplacement de la variable des parametres du referentiel
-                camundaVariables.put("RefParamMPC", refParamMapFiltered);
-            }
-
             // Préparation d'un bilan de résultats
             resultats.add(new ResultatControle()
                     .setCodeControle(codeControle)
@@ -87,7 +69,7 @@ public class ControleService {
         return resultats;
     }
 
-    private Map<String, Object> getDmnInputsAndValues(ParsedDecision decision, Map<String, Object> dmnVariables) {
+    private Map<String, Object> buildDecisionInputsAndValues(ParsedDecision decision, Map<String, Object> dmnVariables) {
         List<String> expressions = camundaService.getDecisionExpressions(decision);
         Map<String, Object> variablesValues = new HashMap<>();
         if(CollectionUtils.isNotEmpty(expressions)) {
